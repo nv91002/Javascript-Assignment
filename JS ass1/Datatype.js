f044c50e4a8eb637efe82773bcc21710dc95b2c3
nv91002 <vaishnvnikhil@gmail.com>
2025-01-24 21:21:47 +0530

    Add files via upload
    Data Types Javascript

---
+++ JS ass1/Datatype.js
@@ -0,0 +1,21 @@
+// Data Types in JS 
+// Using Let 
+let color = "Red"
+console.log(color)
+
+color = "Black"
+console.log(color)
+
+// Declaring var in if block 
+if(true){
+    var game = "Cricket"
+    // let x = "speed"
+}
+console.log(game)
+// console.log(x)
+
+// const variable 
+const fruits =["Mango","Grapes","Banana"]
+fruits.push("Citrus")
+console.log(fruits)
+// fruits = ["Pineapple"] There is Type error in this line as it is a const variable 
