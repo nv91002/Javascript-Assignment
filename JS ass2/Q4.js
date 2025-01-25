6173e04c925b323d198d34fd579668f95c33f750
nv91002 <vaishnvnikhil@gmail.com>
2025-01-25 23:22:51 +0530

    Add files via upload
    Operators and Expressions in JS

---
+++ JS ass2/Q4.js
@@ -0,0 +1,12 @@
+x=10;
+console.log("Initial Value of X:",x)
+x=x+5;
+console.log("After adding 5:",x)
+x=x*3;
+console.log("After Multiplying by 5:",x)
+x=x-10;
+console.log("After subtracting 10: ",x)
+x=x/2;
+console.log("After dividing by 2:",x)
+x=x%3;
+console.log("Modulus of 3:",x)
---
+++ JS ass2/Q5.js
@@ -0,0 +1,8 @@
+x=10+3*5; //In this the multiplication will be performd first beacuse precedence level of * is higher
+y=(10+3)*5; //In this operation within the bracket will be performed first, because the precedence level of () is high
+z=20/2+3; //In this the division will be performd first beacuse precedence level of / is higher
+a=20/(2+3); //In this operation within the bracket will be performed first, because the precedence level of () is high
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(a)
